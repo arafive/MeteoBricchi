b1ad0936f6903308d6150a8c599cbab32240c2ad
Daniele Carnevale <lacaselladidaniele@gmail.com>
2026-07-07 21:01:07 +0200

    Fix no fulmini.csv in sync meteo-dev

--- sync_dati2D.sh
+++ sync_dati2D.sh
@@ -3,23 +3,6 @@
 # Lo rendo eseguibile con: chmod +x sync_meteo-dev.sh
 # Problemi si sync con https://chatgpt.com/share/6a05d1e4-5690-83eb-a0ee-7af4cb8117b6
 
-# Directory dove vuoi salvare i file
-DEST="/run/media/daniele.carnevale/Daniele2TB/repo/MeteoBricchi"
-cd "$DEST" || exit 1
-rsync -rahzPuv --update --modify-window=1 --info=progress2 \
-    --include='*/' \
-    --include='*.png' \
-    --include='*.json' \
-    --include='*.csv' \
-    --exclude='*' \
-    /home/cfmi.arpal.org/daniele.carnevale/Scrivania/MeteoBricchi/dati2D .
-
-echo "$(date): sincronizzazione completata"
-
-
-# Lo rendo eseguibile con: chmod +x sync_meteo-dev.sh
-# Problemi si sync con https://chatgpt.com/share/6a05d1e4-5690-83eb-a0ee-7af4cb8117b6
-
 LOCKFILE="/tmp/sync_dati2D.lock"
 
 # Evita esecuzioni multiple contemporanee
@@ -38,6 +21,7 @@ cd "$DEST" || exit 1
 rsync -rahzPuv --update --modify-window=1 --info=progress2 \
     --include='*/' \
     --include='*.png' \
+    --include='*.csv' \
     --include='*.json' \
     --exclude='*' \
     meteo@meteo-dev:/home/cfmi.arpal.org/meteo/QnapDevMeteo/MeteoBricchi/dati2D .
@@ -49,6 +33,7 @@ cd "$DEST" || exit 1
 rsync -rahzPuv --update --modify-window=1 --info=progress2 \
     --include='*/' \
     --include='*.png' \
+    --include='*.csv' \
     --include='*.json' \
     --exclude='*' \
     meteo@meteo-dev:/home/cfmi.arpal.org/meteo/QnapDevMeteo/MeteoBricchi/dati2D .
